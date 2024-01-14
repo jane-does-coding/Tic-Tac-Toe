f1628aff1ad7b1d3e58f38b0e315b07da5533245
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-01-14 11:01:09 -0700

    sidebar development

---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,13 @@
+<template>
+  <nuxt-link to="/" class="">
+    <div>
+      <slot name="icon"></slot>
+    </div>
+
+    <div>
+      <slot name="name"></slot>
+    </div>
+  </nuxt-link>
+</template>
+
+<script></script>
---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div class="flex flex-col h-screen py-2">
+
+    <div class="p-2 my-0 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20" :class="transition">
+      <nuxt-link to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </nuxt-link>
+    </div>
+
+    <SidebarLeftTab>
+
+      <template v-slot:icon>
+        <HomeIcon />
+      </template>
+
+      <template v-slot:name>
+        Home
+      </template>
+
+    </SidebarLeftTab>
+
+  </div>
+</template>
+
+<script setup>
+  import {HomeIcon} from "@heroicons/vue/24/solid"
+  const {defaultTransition} = useTailwindConfig()
+</script>
