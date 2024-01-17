dd165d2b36b93d7de740627052283dfee4b973aa
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-01-16 21:37:55 -0700

    register user functionality done

--- components/MainSection.vue
+++ components/MainSection.vue
@@ -1,8 +1,8 @@
 
 <template>
-  <div class="border-x" :class="twitterBorderColor">
+  <div class="border-x min-h-screen" :class="twitterBorderColor">
     <div class="sticky top-0 px-4 py-3 bg-white/80 backdrop-blur-md dark:bg-dim-900/80">
-      <h2 class="text-xl font-bold text-gray-800 dark:text-100">{{ props.title }}</h2>
+      <h2 class="text-xl font-bold text-gray-800 dark:text-gray-100">{{ props.title }}</h2>
     </div>
 
     <div v-if="props.loading" class="flex items-center justify-center p-8 border-b" :class="twitterBorderColor">
--- server/api/auth/register.post.js
+++ server/api/auth/register.post.js
@@ -1,4 +1,5 @@
 import { sendError, readBody } from "h3";
+import { createUser } from "~/server/db/users";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
@@ -12,7 +13,21 @@ export default defineEventHandler(async (event) => {
     );
   }
 
+  if (password !== repeatPassword) {
+    createError({ statusCode: 400, statusMessage: "Passwords don't match" });
+  }
+
+  const userData = {
+    username,
+    email,
+    password,
+    name,
+    profileImage: "https://picsum.photos/200/200",
+  };
+
+  const user = await createUser(userData);
+
   return {
-    body: body,
+    body: user,
   };
 });
